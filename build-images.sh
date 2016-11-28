23e0a6b2af916ca2be0beced1bba8417fb08578c
Francis Chuang <francis.chuang@boostport.com>
2016-11-28 21:49:06 +1100

    Bump version.

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=0.1.0
+VERSION=0.2.0
 
 # Build the binaries
 docker run --rm -v $PWD:/go/src/github.com/Boostport/kubernetes-vault -w /go/src/github.com/Boostport/kubernetes-vault golang:1.7-alpine ./build.sh
