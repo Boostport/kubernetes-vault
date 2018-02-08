8c021c13f7131fbc1b079e4464214bb9c2d322ff
Francis Chuang <francis.chuang@boostport.com>
2018-02-08 15:55:18 +1100

    Fix docker entrypoint

--- cmd/controller/docker-entrypoint.sh
+++ cmd/controller/docker-entrypoint.sh
@@ -11,7 +11,7 @@ NUM_ARGS=$#
 
 # If the user is trying to run kubernetes-vault directly with some or no
 # arguments, then pass them to kubernetes-vault.
-if [ "${1%%"${1#?}"}" = '-' ] || [ "$NUM_ARGS" -le 1 ]; then
+if [ "${1%%"${1#?}"}" = '-' ] || [ "$NUM_ARGS" = 0 ] || [ "$NUM_ARGS" = 1 -a "$1" = "version" ]; then
     set -- kubernetes-vault "$@"
 fi
 
